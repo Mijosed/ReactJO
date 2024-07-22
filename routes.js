f414853da3b613537e8da1d5dcfb0f0b1e86d89b
Mijosed <mijosed@gmail.com>
2024-07-22 12:27:24 +0200

    probleme route

--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { HomePage } from './views/HomePage.js';
 import { SportPage } from './views/SportPage.js';
 import { LocationPage } from './views/LocationPage.js';
@@ -12,20 +11,3 @@ const routes = {
 };
 
 export default routes;
-=======
-import HomePage from "./views/home.js";
-import LocationPage from "./views/place.js";
-import SportPage from "./views/sport.js";
-import ErrorPage from "./views/404.js";
-import TestPage from "./views/TestPage.js";
-
-export default {
-    "/": HomePage,
-    "/places": LocationPage,
-    "/sport": SportPage,
-    "/404": ErrorPage,
-    "/test": TestPage,
-
-};
-    
->>>>>>> e92f8e477e84991d8d5873bc81c94a3f5d1d2c40
