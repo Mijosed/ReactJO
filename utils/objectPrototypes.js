f50436167c6480041aacd0c62cc29b598d27df0f
Ludovic MAK <ludovic93mak@gmail.com>
2024-07-23 01:15:36 +0200

    save

--- utils/objectPrototypes.js
+++ utils/objectPrototypes.js
@@ -1,18 +1,18 @@
-Object.prototype.prop_access = function (path) {
-    if (path === null || path === "") {
-        return this;
-    }
+// Object.prototype.prop_access = function (path) {
+//     if (path === null || path === "") {
+//         return this;
+//     }
 
-    const segments = path.match(/[^.[\]]+/g);
-    let current = this;
+//     const segments = path.match(/[^.[\]]+/g);
+//     let current = this;
 
-    for (let i = 0; i < segments.length; i++) {
-        const segment = segments[i];
-        if (current === null || current[segment] === undefined) {
-            throw new Error(`${segments.slice(0, i + 1).join('.')} not exist.`);
-        }
-        current = current[segment];
-    }
+//     for (let i = 0; i < segments.length; i++) {
+//         const segment = segments[i];
+//         if (current === null || current[segment] === undefined) {
+//             throw new Error(`${segments.slice(0, i + 1).join('.')} not exist.`);
+//         }
+//         current = current[segment];
+//     }
 
-    return current;
-};
+//     return current;
+// };
