8d80cfcd219cff237d77b7b140c5a5ac4a2d32b1
Mak Ludovic <ludovic93mak@gmail.com>
2024-07-22 15:08:17 +0200

    save

--- views/Carousel.js
+++ views/Carousel.js
@@ -1,5 +1,5 @@
 import { validateProps } from "../utils/typeCheck.js";
-import { Component } from "../core/Component.js";
+import { Component } from "../core/";
 export class Carousel extends Component {
   #currentIndex;
   #imageState;
@@ -15,18 +15,44 @@ export class Carousel extends Component {
     };
     validateProps(props, propSchema);
     this.#currentIndex = 0;
+    this.#images = props.images;
     this.#imageState = {
-      images: props.images,
+      images: this.#images[this.#currentIndex],
       title: props.title,
     };
-    this.#images = this.#imageState.images
-
   }
 
   render() {
     return {
       tag: "div",
-      children: ["hello carrousel"],
+      props: {
+        class: "image-container",
+      },
+      children: [
+        {
+          tag: "button",
+          props: {
+            class: "carousel-button",
+            onClick: () => this.showPreviousImage(),
+          },
+          children: ["<"],
+        },
+        {
+          tag: "img",
+          props: {
+            src: this.#images[this.currentIndex],
+            alt: "carousel image",
+          },
+        },
+        {
+          tag: "button",
+          props: {
+            class: "carousel-button",
+            onClick: () => this.showNextImage(),
+          },
+          children: [">"],
+        },
+      ],
     };
   }
 }
