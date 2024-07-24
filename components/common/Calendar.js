c1235acf8223f46fea92cb31b285dbc736e1edce
Soumaya <gambonanasoumaya@gmail.com>
2024-07-24 11:57:53 +0200

    Page details d'un sport

--- components/common/Calendar.js
+++ components/common/Calendar.js
@@ -1,6 +1,5 @@
 import { Component } from "../../core/Component.js";
 import { validateProps } from "../../utils/utils.js";
-import { Text } from "../elements/Text.js";
 
 export class Calendar extends Component {
     #day;
@@ -27,70 +26,63 @@ export class Calendar extends Component {
         this.#category = props.category;
         this.#location = props.location;
         validateProps(props, propSchema);
+    }
 
-        // Initialisation des instances du composant Text
-        this.textDay = new Text({ text: this.#day });
-        this.textMonth = new Text({ text: this.#month });
-        this.textTime = new Text({ text: this.#time });
-        this.textCategory = new Text({ text: this.#category });
-        this.textLocation = new Text({ text: this.#location });
+    createCalendarDiv() {
+        return {
+            tag: "div",
+            props: { class: "flex bg-white shadow border", id: "calendar",
+                style: "border-radius: 10px; display: flex; flex-direction: row; height: 150px" },
+            children: [
+                {
+                    tag: "div",
+                    props: { class: "object-cover bg-[#0078D0] h-full p-2.5 text-[#FFB114] rounded-l-[10px]",
+                        style: "text-align: center;" },
+                    children: [
+                        { tag: "div", props: {}, children: ["Vendredi"] },
+                        { tag: "div", props: { class: "text-white text-2xl" }, children: [this.#day] },
+                        { tag: "div", props: {}, children: [this.#month] },
+                    ]
+                },
+                {
+                    tag: "div",
+                    props: { style: "text-align: left; padding: 10px; width: 400px" },
+                    children: [
+                        { tag: "div", props: { class: "simple-text" }, children: ["A 20km de Paris"] },
+                        { tag: "div", props: {}, children: [this.#time] },
+                        { tag: "div", props: { class: "text-[#9E9E9E]" }, children: [this.#category] },
+                        {
+                            tag: "div",
+                            props: { class: "mb-2 flex items-center" },
+                            children: [
+                                {
+                                    tag: "img",
+                                    props: {
+                                        src: "../assets/icons/position-bl.svg",
+                                        alt: "Position Icon",
+                                        class: "w-6 h-6 mr-2"
+                                    },
+                                    children: []
+                                },
+                                this.#location,
+                            ],
+                        }
+                    ]
+                },
+            ],
+        };
     }
 
     render() {
         return {
             tag: "div",
-            props: { class: "overflow-hidden mx-40 my-5 flex", style: "display: flex; align-items: center;" },
+            props: { class: "overflow-hidden mx-40 my-5 flex", style: "display: flex;" },
             children: [
                 {
                     tag: "div",
-                    props: { class: "flex bg-white shadow border ",
-                             style: "border-radius: 10px; display: flex; flex-direction: row; height: 150px" },
+                    props: { class: "flex flex-col", style: "flex: 1;" },
                     children: [
-                        {
-                            tag: "div",
-                            props: {class: "object-cover bg-[#0078D0] h-full p-2.5 text-[#FFB114] rounded-l-[10px]",
-                                    style: "text-align: center;" },
-                            children: [
-                                { tag: "div", props: {}, children: ["Vendredi"] },
-                                { tag: "div", props: {class: " text-white"}, children: [this.#day] },
-                                { tag: "div", props: {}, children: [this.#month] },
-                            ]
-                        },
-                        {
-                            tag: "div",
-                            props: { class: "",
-                                style: "text-align: left; padding: 10px; width: 400px"},
-                            children: [
-                                { tag: "div", props: { class: "simple-text" }, children: ["A 20km de Paris"] },
-                                { tag: "div", props: {}, children: [this.#time] },
-                                { tag: "div", props: { class: " text-[#9E9E9E]" }, children: [this.#category] },
-                                {
-                                    tag: "div",
-                                    props: { class: "mb-2 flex items-center" },
-                                    children: [
-                                        {
-                                            tag: "img",
-                                            props: {
-                                                src: "../assets/icons/position-bl.svg",
-                                                alt: "Position Icon",
-                                                class: "w-6 h-6 mr-2"
-                                            },
-                                            children: []
-                                        },
-                                        this.#location,
-                                    ],
-                                }
-                            ]
-                        },
-                    ],
-                },
-
-                {
-                    tag: "div",
-                    props: { class: "flex-1 p-4 bg-white-200 flex flex-col items-center" },
-                    children: [
-                        { tag: "img", props: { src: "../../assets/images/icon-natation.png", alt: "Image 1", class: "mb-4 object-cover" } },
-                        { tag: "img", props: { src: "../../assets/images/mascot.svg", alt: "Image 2", class: " object-cover" } }
+                        this.createCalendarDiv(),
                     ],
                 }
             ],
--- views/SportPage.js
+++ views/SportPage.js
@@ -8,7 +8,6 @@ import {
 } from '../components/Components.js';
 import { validateProps } from '../utils/utils.js';
 
-
 export class SportPage extends Component {
     constructor(props) {
         super(props);
@@ -32,20 +31,27 @@ export class SportPage extends Component {
                 { label: props.title, href: "#" }
             ]
         });
-        const calendarProps = {
+
+        // Instances de Calendar avec des propriétés différentes
+        this.calendar1 = new Calendar({
             day: "26",
             month: "Juillet",
             time: "10:00",
             category: "Homme",
             location: "Trocadero",
-        };
+        });
+        this.calendar2 = new Calendar({
+            day: "27",
+            month: "Août",
+            time: "14:00",
+            category: "Femme",
+            location: "Complexe sportif Paris La Défense Arena",
+        });
 
-        this.textHistory = new Text({ text: "La naissance de la natation remonte à la préhistoire, mais il faut attendre le 19e siècle pour que sa pratique devienne compétitive. La Société nationale britannique de natation est créée au début du siècle, et s’occupe d’organiser les premières compétitions. Celles-ci se pratiquent à l’époque en brasse, ou en une nage approchante ; elles se sont ensuite enrichies d’une grande variété de disciplines, aujourd’hui pratiquées aux Jeux Olympiques." +
-                "La natation est une discipline historique des Jeux Olympiques de l’ère moderne. Si les premières courses olympiques se déroulaient en environnement naturel, dès les Jeux de Londres en 1908, les épreuves ont pris place dans une piscine, ce qui a donné lieu à la création de la Fédération Internationale de Natation (FINA). La nage libre et la brasse sont les seules épreuves présentes aux Jeux d’Athènes en 1896, le dos est ensuite ajouté en 1904, puis le papillon apparaît en 1956 aux Jeux de Melbourne." });
+        this.textHistory = new Text({ text: "La naissance de la natation remonte à la préhistoire, mais il faut attendre le 19e siècle pour que sa pratique devienne compétitive. La Société nationale britannique de natation est créée au début du siècle, et s’occupe d’organiser les premières compétitions. Celles-ci se pratiquent à l’époque en brasse, ou en une nage approchante; elles se sont ensuite enrichies d’une grande variété de disciplines, aujourd’hui pratiquées aux Jeux Olympiques. La natation est une discipline historique des Jeux Olympiques de l’ère moderne. Si les premières courses olympiques se déroulaient en environnement naturel, dès les Jeux de Londres en 1908, les épreuves ont pris place dans une piscine, ce qui a donné lieu à la création de la Fédération Internationale de Natation (FINA). La nage libre et la brasse sont les seules épreuves présentes aux Jeux d’Athènes en 1896, le dos est ensuite ajouté en 1904, puis le papillon apparaît en 1956 aux Jeux de Melbourne." });
+
         this.titleLineSportCalender = new TitleLine({ title: "CALENDRIER OLYMPIQUE" });
-        this.calendar = new Calendar(calendarProps);
         this.titleLineSport = new TitleLine({ title: "HISTOIRE" });
-
     }
 
     render() {
@@ -54,7 +60,28 @@ export class SportPage extends Component {
             children: [
                 this.headerSport.render(),
                 this.titleLineSportCalender.render(),
-                this.calendar.render(),
+                {
+                    tag: "div",
+                    props: { class: "flex", style: "display: flex;" },
+                    children: [
+                        {
+                            tag: "div",
+                            props: { class: "flex flex-col", style: "flex: 1;" },
+                            children: [
+                                this.calendar1.render(),
+                                this.calendar2.render(),
+                            ],
+                        },
+                        {
+                            tag: "div",
+                            props: { class: "flex-1 p-4 bg-white-200 flex flex-row items-center", style: "flex: 1;" },
+                            children: [
+                                { tag: "img", props: { src: "../../assets/images/icon-natation.png", alt: "Image 1", class: "mb-4 object-cover mr-4", width: "200" } },
+                                { tag: "img", props: { src: "../../assets/images/mascot.svg", alt: "Image 2", class: "object-cover", width: "200" } }
+                            ],
+                        },
+                    ],
+                },
                 this.titleLineSport.render(),
                 this.textHistory.render(),
                 this.footer.render(),
@@ -62,4 +89,3 @@ export class SportPage extends Component {
         };
     }
 }
-
