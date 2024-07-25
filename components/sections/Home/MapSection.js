40a79faaf5fc966ad204805cea10cf27f9cdef41
Mak Ludovic <ludovic93mak@gmail.com>
2024-07-25 14:18:56 +0200

    save

--- components/sections/Home/MapSection.js
+++ components/sections/Home/MapSection.js
@@ -1,113 +1,84 @@
-import { Component } from '../../../core/Component.js';
-import { SearchBar, FilterButton, MapSearchMenu } from '../../Components.js';
+import { Component } from "../../../core/Component.js";
+import { SearchBar, FilterButton, MapSearchMenu } from "../../Components.js";
 
 export class MapSection extends Component {
-    constructor(props = {}) {
-        super(props);
-        this.isMenuOpen = false;
-        this.SearchBar = new SearchBar( { id: "search-bar-map" } );
-        this.FilterButton = new FilterButton();
-        this.MapSearchMenu = new MapSearchMenu();
+  constructor(props = {}) {
+    super(props);
+    this.SearchBar = new SearchBar({ id: "search-bar-map" });
+    this.FilterButton = new FilterButton();
+    this.MapSearchMenu = new MapSearchMenu({ id: "map-search-menu", state: { displayMenu: this.state.displayMenu } });
+  }
+
+  toggleMenu() {}
+  openMenu() {
+    debugger;
+    this.MapSearchMenu.setState({ displayMenu: true });
+  }
+
+  handleOutsideClick(event) {
+    const menuElement = document.getElementById("map-search-menu");
+    const toggleButton = document.getElementById("menu-toggle-button");
+    if (this.isMenuOpen && !menuElement.contains(event.target) && event.target !== toggleButton) {
+      this.toggleMenu(event);
     }
+  }
 
-    toggleMenu(event) {
-        event.stopPropagation();
-        this.isMenuOpen = !this.isMenuOpen;
-        const menuElement = document.getElementById('map-search-menu');
-        const searchElement = document.getElementById('search');
-        const toggleButton = document.getElementById('menu-toggle-button');
-        const toggleIcon = document.getElementById('toggle-icon');
+  componentDidMount() {
+    document.addEventListener("click", this.handleOutsideClick.bind(this));
+  }
 
-        if (this.isMenuOpen) {
-            menuElement.style.display = 'block';
-            setTimeout(() => {
-                const menuWidth = menuElement.offsetWidth;
-                menuElement.classList.remove('-translate-x-full');
-                menuElement.classList.add('translate-x-0');
-                menuElement.style.transition = 'transform 0.4s ease-in-out';
-                toggleButton.style.transform = `translateX(${menuWidth}px)`;
-                toggleIcon.style.transform = 'rotate(180deg)';
-            }, 2);
-            searchElement.style.display = 'none';
-        } else {
-            setTimeout(() => {
-                menuElement.classList.remove('translate-x-0');
-                menuElement.classList.add('-translate-x-full');
-                menuElement.style.transition = 'transform 0.4s ease-in-out';
-                toggleButton.style.transform = 'translateX(0)';
-                toggleIcon.style.transform = 'rotate(0deg)';
-                searchElement.style.display = 'flex';
-            }, 2);
-        }
-    }
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleOutsideClick.bind(this));
+  }
 
-    handleOutsideClick(event) {
-        const menuElement = document.getElementById('map-search-menu');
-        const toggleButton = document.getElementById('menu-toggle-button');
-        if (this.isMenuOpen && !menuElement.contains(event.target) && event.target !== toggleButton) {
-            this.toggleMenu(event);
-        }
-    }
-
-    componentDidMount() {
-        document.addEventListener('click', this.handleOutsideClick.bind(this));
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener('click', this.handleOutsideClick.bind(this));
-    }
-
-    render() {
-        return {
-            tag: "div",
-            props: { id: "map-section", class: "relative" },
-            children: [
+  render() {
+    return {
+      tag: "div",
+      props: { id: this.props.id, class: "relative" },
+      children: [
+        {
+          tag: "div",
+          props: {
+            class: "absolute top-1/2 transform -translate-y-1/2 z-50 transition-all duration-150 ease-in-out",
+            style: "left: 0;",
+          },
+          children: [
+            {
+              tag: "button",
+              props: {
+                id: "menu-toggle-button",
+                class: "bg-white text-black rounded-r-xl py-6 shadow-lg",
+                onClick: this.toggleMenu.bind(this),
+                style: "transition: transform 0.4s ease-in-out;",
+              },
+              children: [
                 {
-                    tag: "div",
-                    props: { 
-                        class: "absolute top-1/2 transform -translate-y-1/2 z-50 transition-all duration-150 ease-in-out", 
-                        style: "left: 0;" 
-                    },
-                    children: [
-                        {
-                            tag: "button",
-                            props: {
-                                id: "menu-toggle-button",
-                                class: "bg-white text-black rounded-r-xl py-6 shadow-lg",
-                                onClick: this.toggleMenu.bind(this),
-                                style: "transition: transform 0.4s ease-in-out;",
-                            },
-                            children: [
-                                {
-                                    tag: "img",
-                                    props: {
-                                        id: "toggle-icon",
-                                        src: "../assets/icons/triangle.svg",
-                                        alt: "Toggle Menu",
-                                        class: "w-6 h-6",
-                                        style: "transition: transform 0.4s ease-in-out;"
-                                    },
-                                    children: []
-                                }
-                            ]
-                        }
-                    ]
+                  tag: "img",
+                  props: {
+                    id: "toggle-icon",
+                    src: "../assets/icons/triangle.svg",
+                    alt: "Toggle Menu",
+                    class: "w-6 h-6",
+                    style: "transition: transform 0.4s ease-in-out;",
+                  },
+                  children: [],
                 },
-                this.MapSearchMenu.render(),
-                {
-                    tag: "div",
-                    props: { id: "search", class: "absolute flex justify-center z-40 w-full" },
-                    children: [
-                        this.FilterButton.render(),
-                        this.SearchBar.render(),
-                    ]
-                },
-                {
-                    tag: "div",
-                    props: { id: "map", class: "h-[85vh] z-10", style: "width: 100%;" },
-                    children: []
-                }
-            ]
-        };
-    }
+              ],
+            },
+          ],
+        },
+        this.MapSearchMenu.render(),
+        {
+          tag: "div",
+          props: { id: "search", class: "absolute flex justify-center z-40 w-full" },
+          children: [this.FilterButton.render(), this.SearchBar.render()],
+        },
+        {
+          tag: "div",
+          props: { id: "map", class: "h-[85vh] z-10", style: "width: 100%;" },
+          children: [],
+        },
+      ],
+    };
+  }
 }
--- views/HomePage.js
+++ views/HomePage.js
@@ -1,163 +1,153 @@
-import { Component } from '../core/Component.js';
-import {
-    HeaderHome,
-    MapSection,
-    Footer,
-    HomeTitle,
-    SportSection
-} from '../components/Components.js';
-import { validateProps } from '../utils/typeCheck.js';
-import { fetchData } from '../api/fetchData.js';
+import { Component } from "../core/Component.js";
+import { HeaderHome, MapSection, Footer, HomeTitle, SportSection } from "../components/Components.js";
+import { validateProps } from "../utils/typeCheck.js";
+import { fetchData } from "../api/fetchData.js";
 
 export class HomePage extends Component {
-    constructor(props) {
-        super(props);
-        const propSchema = {
-            type: 'object',
-            properties: {
-                title: { type: 'string' }
-            }
-        };
-        validateProps(props, propSchema);
+  constructor(props) {
+    super(props);
+    const propSchema = {
+      type: "object",
+      properties: {
+        title: { type: "string" },
+      },
+    };
+    validateProps(props, propSchema);
 
-        this.state = {
-            sports: [],
-            mapInitialized: false,
-            error: null
-        };
+    this.state = {
+      sports: [],
+      mapInitialized: false,
+      error: null,
+    };
 
-        this.headerHome = new HeaderHome();
-        this.titleElementSites = new HomeTitle({ text: "Explorer les sites", couleur: "white", id: "sites", textColor: "black" });
-        this.mapElement = new MapSection();
-        this.footerElement = new Footer();
-        this.sportsSection = new SportSection({ id: "sports-section", sports: this.state.sports });
-        this.componentDidMount();
-    }
+    this.headerHome = new HeaderHome();
+    this.titleElementSites = new HomeTitle({ text: "Explorer les sites", couleur: "white", id: "sites", textColor: "black" });
+    this.mapElement = new MapSection({
+      id: "map-section",
+      state: {
+        displayMenu: false,
+      },
+    });
+    this.footerElement = new Footer();
+    this.sportsSection = new SportSection({ id: "sports-section", sports: this.state.sports });
+    this.componentDidMount();
+  }
+  openMenu(event) {
+    this.mapElement.openMenu();
+  }
 
-    async componentDidMount() {
-        try {
-            const data = await fetchData();
-            const sports = data.sports || [];
-            this.setState({ sports });
+  async componentDidMount() {
+    try {
+      const data = await fetchData();
+      const sports = data.sports || [];
+      this.setState({ sports });
 
-            const map = L.map('map', { gestureHandling: true }).setView([48.8566, 2.3522], 12);
+      const map = L.map("map", { gestureHandling: true }).setView([48.8566, 2.3522], 12);
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
 
-            const olympicSites = data.results.map(site => ({
-                coords: [site.point_geo.lat, site.point_geo.lon],
-                name: site.nom_site
-            }));
+      const olympicSites = data.results.map((site) => ({
+        coords: [site.point_geo.lat, site.point_geo.lon],
+        name: site.nom_site,
+      }));
 
-            olympicSites.forEach(site => {
-                L.marker(site.coords).addTo(map)
-                    .bindPopup(site.name)
-                    .openPopup();
-            });
+      olympicSites.forEach((site) => {
+        const marker = L.marker(site.coords).addTo(map).bindPopup(site.name).openPopup();
+        marker.on("click", (event) => this.openMenu(event));
+      });
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition((position) => {
-                    const { latitude, longitude } = position.coords;
-                    map.setView([latitude, longitude], 12);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const { latitude, longitude } = position.coords;
+          map.setView([latitude, longitude], 12);
 
-                    let customIcon = L.icon({
-                        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-                        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                        iconSize: [25, 41],
-                        iconAnchor: [12, 41],
-                        popupAnchor: [1, -34],
-                    });
+          let customIcon = L.icon({
+            iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+            shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+          });
 
-                    L.marker([latitude, longitude], { icon: customIcon })
-                        .addTo(map)
-                        .bindPopup('Je suis géolocalisé(e) !')
-                        .openPopup();
+          L.marker([latitude, longitude], { icon: customIcon }).addTo(map).bindPopup("Je suis géolocalisé(e) !").openPopup();
 
-                    // Vérifie la proximité du site le plus proche
-                    this.checkProximity([latitude, longitude], olympicSites);
-                });
-            }
-
-            this.setState({ mapInitialized: true });
-        } catch (error) {
-            this.setState({ error: "Erreur lors de la récupération des données : " + error.message });
-            console.error("Erreur lors de la récupération des données :", error);
-        }
-    }
-
-    checkProximity(userCoords, sites) {
-        let closestSite = null;
-        let minDistance = Infinity;
-
-        sites.forEach(site => {
-            const distance = this.getDistance(userCoords, site.coords);
-            if (distance < minDistance) {
-                minDistance = distance;
-                closestSite = site;
-            }
+          // Vérifie la proximité du site le plus proche
+          this.checkProximity([latitude, longitude], olympicSites);
         });
+      }
 
-        if (closestSite && minDistance < 50) { 
-            this.sendNotification(closestSite.name, minDistance);
+      this.setState({ mapInitialized: true });
+    } catch (error) {
+      this.setState({ error: "Erreur lors de la récupération des données : " + error.message });
+      console.error("Erreur lors de la récupération des données :", error);
+    }
+  }
+
+  checkProximity(userCoords, sites) {
+    let closestSite = null;
+    let minDistance = Infinity;
+
+    sites.forEach((site) => {
+      const distance = this.getDistance(userCoords, site.coords);
+      if (distance < minDistance) {
+        minDistance = distance;
+        closestSite = site;
+      }
+    });
+
+    if (closestSite && minDistance < 50) {
+      this.sendNotification(closestSite.name, minDistance);
+    }
+  }
+
+  getDistance(coord1, coord2) {
+    const R = 6371;
+    const dLat = ((coord2[0] - coord1[0]) * Math.PI) / 180;
+    const dLon = ((coord2[1] - coord1[1]) * Math.PI) / 180;
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos((coord1[0] * Math.PI) / 180) * Math.cos((coord2[0] * Math.PI) / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return R * c;
+  }
+
+  sendNotification(siteName, distance) {
+    const roundedDistance = Math.round(distance * 100) / 100;
+    const message = `Le site de compétition le plus proche est ${siteName}, situé à ${roundedDistance} km.`;
+    if (Notification.permission === "granted") {
+      new Notification("Site de compétition à proximité", {
+        body: message,
+      });
+    } else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification("Site de compétition à proximité", {
+            body: message,
+          });
         }
+      });
+    }
+  }
+
+  render() {
+    const { sports, mapInitialized, error } = this.state;
+
+    if (error) {
+      return {
+        tag: "div",
+        props: { class: "error-message" },
+        children: [error],
+      };
     }
 
-    getDistance(coord1, coord2) {
-        const R = 6371; 
-        const dLat = (coord2[0] - coord1[0]) * Math.PI / 180;
-        const dLon = (coord2[1] - coord1[1]) * Math.PI / 180;
-        const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                  Math.cos(coord1[0] * Math.PI / 180) * Math.cos(coord2[0] * Math.PI / 180) *
-                  Math.sin(dLon/2) * Math.sin(dLon/2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return R * c; 
-    }
-
-    sendNotification(siteName, distance) {
-        const roundedDistance = Math.round(distance * 100) / 100; 
-        const message = `Le site de compétition le plus proche est ${siteName}, situé à ${roundedDistance} km.`;
-        if (Notification.permission === 'granted') {
-            new Notification('Site de compétition à proximité', {
-                body: message,
-            });
-        } else if (Notification.permission !== 'denied') {
-            Notification.requestPermission().then(permission => {
-                if (permission === 'granted') {
-                    new Notification('Site de compétition à proximité', {
-                        body: message,
-                    });
-                }
-            });
-        }
-    }
-
-    render() {
-        const { sports, mapInitialized, error } = this.state;
-
-        if (error) {
-            return {
-                tag: "div",
-                props: { class: "error-message" },
-                children: [error]
-            };
-        }
-
-        return {
-            tag: "div",
-            children: [
-                this.headerHome.render(),
-                this.titleElementSites.render(),
-                this.mapElement.render(),
-                this.sportsSection.render(),
-                this.footerElement.render()
-            ]
-        };
-    }
+    return {
+      tag: "div",
+      children: [this.headerHome.render(), this.titleElementSites.render(), this.mapElement.render(), this.sportsSection.render(), this.footerElement.render()],
+    };
+  }
 }
 
 export default function renderHomePage() {
-    const homePage = new HomePage({ title: "Home Page" });
-    return homePage.renderDOM();
+  const homePage = new HomePage({ title: "Home Page" });
+  return homePage.renderDOM();
 }
